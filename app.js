314842b186a8947270658dfd6d537e8a40820f76
Jay Kumar <jay.sivan.kumar@gmail.com>
2018-02-05 13:20:30 -0800

    Fixed href link; added javascript to add new members

---
+++ app.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $("#joinbutton").on('click', function() {
+        var new_member_name = $("#newmember").val();
+        var li = '<li>' + new_member_name + '</li>';
+        var ul = $("#memberlist")
+        ul.append(li);
+    });
+});
