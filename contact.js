50e36208ae0a9133eb8ee0366c6a0ce5c9c654fc
amamer401 <amamer401@gamil.com>
2018-02-17 19:23:57 -0500

    church website2

--- contact.js
+++ contact.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-var button = document.querySelectorByClass("btn");
-
-button.addEventListener("click" , function(){
-
-	alert("Thank u we will contact u ASAP");
-
-=======
 $('#btn').on('click', function (event) {
     console.log("clicked");
     alert("Thank u we will contact u ASAP");
->>>>>>> 879f70662201f7f5ac7f0cd6b4fb2818ea14f921
+
 });
